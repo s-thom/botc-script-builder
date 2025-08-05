2bd64d26566da4e9fd654001e310f3f4bcc247cf
Stuart Thomson <me@sthom.kiwi>
2025-08-05 21:11:08 +1200

    Change state type

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
   import tb from "../generated/edition-scripts/tb.json";
-  import { getFullScriptCharacter, isScriptMetadata } from "../lib/characters";
-  import { scriptState } from "../lib/state.svelte";
+  import { globalState, setScript } from "../lib/state.svelte";
   import CharacterIcon from "./common/CharacterIcon.svelte";
-
-  const meta = $derived(
-    scriptState.script.find((item) => isScriptMetadata(item))
-  );
-  const characters = $derived(
-    scriptState.script
-      .filter((item) => !isScriptMetadata(item))
-      .map((item) => getFullScriptCharacter(item))
-  );
 </script>
 
 <main>
@@ -19,14 +9,14 @@
     <button
       type="button"
       onclick={() => {
-        scriptState.script = tb;
+        setScript(tb);
       }}>set script to tb</button
     >
   </div>
   <div>
     <div></div>
     <ul>
-      {#each characters as character}
+      {#each globalState.characters as character}
         <li>
           <CharacterIcon {character} />
           <h3>{character.name}</h3>
--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,9 +1,26 @@
-import type { BloodOnTheClocktowerCustomScript } from "../generated/script-schema";
+import type {
+  BloodOnTheClocktowerCustomScript,
+  ScriptCharacter,
+  ScriptMetadata,
+} from "../generated/script-schema";
+import { getFullScriptCharacter, isScriptMetadata } from "./characters";
 
-export const scriptState = $state<{ script: BloodOnTheClocktowerCustomScript }>(
-  { script: [] }
-);
+export interface GlobalState {
+  meta: ScriptMetadata;
+  characters: ScriptCharacter[];
+}
+
+export const globalState = $state<GlobalState>({
+  meta: { id: "_meta", name: "" },
+  characters: [],
+});
 
 export function setScript(script: BloodOnTheClocktowerCustomScript) {
-  scriptState.script = script;
+  const meta = script.find((item) => isScriptMetadata(item));
+  const characters = script
+    .filter((item) => !isScriptMetadata(item))
+    .map((item) => getFullScriptCharacter(item));
+
+  globalState.meta = meta ?? { id: "_meta", name: "" };
+  globalState.characters = characters;
 }
