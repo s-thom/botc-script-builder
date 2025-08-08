76e3bb491b7d3c78fb4321cc37a109c7b79e3bc8
Stuart Thomson <me@sthom.kiwi>
2025-08-08 21:24:14 +1200

    Split UI horizontally

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,20 +4,35 @@
   import CurrentCharacterList from "./CurrentCharacterList.svelte";
 </script>
 
-<main>
-  <div>
+<main class="container">
+  <div class="column character-list">
     <button
       type="button"
       onclick={() => {
         setScript(tb);
       }}>set script to tb</button
     >
-  </div>
-  <div>
     <div></div>
     <CurrentCharacterList />
   </div>
+  <div class="column">dsfjklasdfjklsdfkl</div>
+  <div class="column">errterwerwerwe</div>
 </main>
 
 <style>
+  .container {
+    height: 100vh;
+    max-height: 100vh;
+    display: flex;
+  }
+
+  .column {
+    height: 100%;
+    max-height: 100%;
+    overflow-y: auto;
+  }
+
+  .character-list {
+    width: 300px;
+  }
 </style>
